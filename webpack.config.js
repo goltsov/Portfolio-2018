2f34c71d77d2c2c2f6d335742d93afa462070f41
Unknown <goltsov.k@gmail.com>
2018-04-29 19:37:30 -0400

    switch sass compiling from npm scripts to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path');
 
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "../css/[name].css",
+    disable: process.env.NODE_ENV === "development"
+});
+
 module.exports = {
   entry: './src/js/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist/js')
-  }
+  },
+  module: {
+      rules: [{
+          test: /\.scss$/,
+          use: extractSass.extract({
+              use: [{
+                  loader: "css-loader"
+              }, {
+                  loader: "sass-loader"
+              }],
+              // use style-loader in development
+              fallback: "style-loader"
+          })
+      }]
+  },
+  plugins: [
+      extractSass
+  ]
 };
